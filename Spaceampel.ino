5218ae65cd611f110ef38739ce577e2b94c84d4a
Pixtxa <30337073+Pixtxa@users.noreply.github.com>
2020-02-18 20:18:16 +0100

    Verbesserter Code für Telegram, FOTA
    Joa, gibt noch immer keine hübsche Lösung, aber zumindest sollte die Bastelllösung nun keine Meldungen mehr triggern, wenn ein MQTT-Paket rein kommt, das den selben Zustand wie zuvor enthält.
    
    Und FOTA sollte nun auch gehen.

--- Spaceampel.ino
+++ Spaceampel.ino
@@ -1,16 +1,32 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ESP8266HTTPClient.h>
+#include <Ticker.h>
+#include <ArduinoOTA.h>
 
-const char* SSID = "vspace.one";
-const char* PSK = "12345678";
-const char* MQTT_BROKER = "mqtt.vspace";
+const char* SSID = "SSID";
+const char* PSK = "PASSWORD";
+const char* MQTT_BROKER = "BROKER_DNS";
+#define UPDATEPW "UPDATE_PASSWORD"
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
+//59c3dc1a8b797
+int opened = -1;
+int opened_old = -1;
+bool tick_flag = 0;
+int transmission_delay = 0;
+
+Ticker ticker;
+void tick() {
+  tick_flag = true;
+  if (transmission_delay > 0) {
+    transmission_delay--;
+  }
+}
 
 bool telegram(String Message = "[SpaceAmpel_Fehler: Kein Text an Senderoutine]", String Token = "59c3dc1a8b797") {
   int c;
@@ -36,6 +52,8 @@ bool telegram(String Message = "[SpaceAmpel_Fehler: Kein Text an Senderoutine]",
   return httpCode != HTTP_CODE_OK;
 }
 
+
+
 void setup() {
   //********** CHANGE PIN FUNCTION  TO GPIO **********
   //GPIO 1 (TX) swap the pin to a GPIO.
@@ -51,6 +69,57 @@ void setup() {
   setup_wifi();
   client.setServer(MQTT_BROKER, 1883);
   client.setCallback(callback);
+
+  
+  
+  
+  ArduinoOTA.setPort(8266);
+  ArduinoOTA.setHostname("SpaceAmpel");
+#ifdef UPDATEPW
+  ArduinoOTA.setPassword(UPDATEPW);
+#endif
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    //Serial.println("Start updating " + type);
+    //telegram("SpaceAmpel:\nStart updating " + type + " …");
+  });
+  ArduinoOTA.onEnd([]() {
+    //Serial.println("\nEnd");
+    telegram("SpaceAmpel: Firmwareupdate abgeschlossen!");
+  });
+  //ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+  //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  //});
+  ArduinoOTA.onError([](ota_error_t error) {
+    //Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      //Serial.println("Auth Failed");
+      telegram("SpaceAmpel:\nFOTA_ERROR: Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      //Serial.println("Begin Failed");
+      telegram("SpaceAmpel:\nFOTA_ERROR: Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      //Serial.println("Connect Failed");
+      telegram("SpaceAmpel:\nFOTA_ERROR: Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      //Serial.println("Receive Failed");
+      telegram("SpaceAmpel:\nFOTA_ERROR: Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      //Serial.println("End Failed");
+      telegram("SpaceAmpel:\nFOTA_ERROR: End Failed");
+    }
+    ESP.restart();
+  });
+  ArduinoOTA.begin();
+  ticker.attach_ms(50, tick);
 }
 
 void setup_wifi() {
@@ -62,6 +131,7 @@ void setup_wifi() {
   digitalWrite(2, HIGH);
   digitalWrite(1, LOW);
 
+  WiFi.hostname("SpaceAmpel");
   WiFi.begin(SSID, PSK);
   int i = 9;
   while (WiFi.status() != WL_CONNECTED) {
@@ -113,15 +183,18 @@ void callback(char* topic, byte* payload, unsigned int length) {
   if (strcmp(msg, "{\"status\":\"ok\",\"data\":{\"open\":true}}") == 0) {
     digitalWrite(0, HIGH);
     digitalWrite(2, LOW);
-    telegram("vspace.one geöffnet");
+    opened = 1;
+//    telegram("vspace.one geöffnet");
     //        Serial.println("Offen");
   }
   else if (strcmp(msg, "{\"status\":\"ok\",\"data\":{\"open\":false}}") == 0) {
     digitalWrite(0, LOW);
     digitalWrite(2, HIGH);
-    telegram("vspace.one geschlossen");
+    opened = 0;
+//    telegram("vspace.one geschlossen");
     //        Serial.println("Zu");
   }
+  transmission_delay = 200;
 }
 
 void reconnect() {
@@ -143,4 +216,18 @@ void loop() {
     reconnect();
   }
   client.loop();
+  ArduinoOTA.handle();
+
+  if (tick_flag){
+    tick_flag = false;
+
+    if (opened != opened_old && transmission_delay == 0){
+      opened_old = opened;
+      if (opened == 1){
+        telegram("vspace.one geöffnet");
+      }else{
+        telegram("vspace.one geschlossen");      
+      }
+    }
+  }
 }
